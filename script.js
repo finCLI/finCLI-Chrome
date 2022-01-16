eb0387093cb556cac1cb1d86d6d295c00202520f
Dhairya <dhairya.ostwal@gmail.com>
2022-01-16 10:41:52 +0530

    API Working. More news to be added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-fetch('https://fincli-news-api.herokuapp.com/india/', {
+fetch('https://fincli-news-api.herokuapp.com/india', {
   method: 'GET',
-  // body: JSON.stringify(),
   cache: 'default',
-  // mode: 'no-cors',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // headers: {
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Methods': 'GET',
+  // },
 })
   .then((data) => data.json())
   .then((newsData) => {
@@ -14,8 +14,47 @@ fetch('https://fincli-news-api.herokuapp.com/india/', {
     const newsLink = newsData[0].news_link;
     const publisher = newsData[0].news_publisher;
 
-    const newsElement = document.getElementById('newsElement');
+    const publisherElement = document.getElementById('publisherElement');
+    publisherElement.innerHTML = publisher;
 
-    newsElement.innerHTML = headline;
+    const headlineElement = document.getElementById('headlineElement');
+    headlineElement.innerHTML = headline;
+    headlineElement.setAttribute('href', newsLink);
+
+    document.getElementById('newsIDElement').innerHTML =
+      newsData[0].id + 1 + '. ';
+
+    // adding further news headlines
+
+    var len = newsData.length;
+    console.log(len);
+
+    len = Math.min(len, 5);
+
+    // for (var i = 1; i < len; i++) {
+    // const divEnclosingNews = document.createElement("div")
+    const newsBlock = document.createElement('div');
+    const np = document.createElement('h3');
+    const node = document.createTextNode(newsData[1].news_publisher);
+    np.appendChild(node);
+
+    const nh = document.createElement('a');
+    const nh_text = document.createTextNode(newsData[1].news_headline);
+    const nh_link = document.createTextNode(newsData[1].news_link);
+    // nh.setAttribute("href", nh_link);
+    nh.href = nh_link;
+
+    nh.appendChild(nh_text);
+    newsBlock.append(np);
+    newsBlock.append(nh);
+
+    // var h = newsData[1].news_headline;
+    // console.log(typeof h);
+
+    // const nh_text = document.createTextNode(h);
+    // newsBlock.appendChild(nh_text);
+    const element = document.getElementById('IN-News');
+    element.appendChild(newsBlock);
+    // }
   })
   .catch((Error) => console.log(Error));
