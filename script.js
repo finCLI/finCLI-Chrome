b1f9b8050ce5e880df91f705caf12a5b7ba6ab27
Dhairya <dhairya.ostwal@gmail.com>
2023-11-05 18:27:58 +0530

    Fixed hyperlinks + POST Call

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
-fetch('https://fincli-news-api.herokuapp.com/india', {
-  method: 'GET',
-  cache: 'default',
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Access-Control-Allow-Methods': 'GET',
-  // },
-})
-  .then((data) => data.json())
-  .then((newsData) => {
-    // console.log(newsData);
-    const headline = newsData[0].news_headline;
-    const newsLink = newsData[0].news_link;
-    const publisher = newsData[0].news_publisher;
-
-    const publisherElement = document.getElementById('publisherElement');
-    publisherElement.innerHTML = publisher;
-
-    const headlineElement = document.getElementById('headlineElement');
-    headlineElement.innerHTML = headline;
-    headlineElement.setAttribute('href', newsLink);
-
-    document.getElementById('newsIDElement').innerHTML =
-      newsData[0].id + 1 + '. ';
-
-    // adding further news headlines
-
-    var len = newsData.length;
-    console.log(len);
-
-    len = Math.min(len, 5);
-
-    for (var i = 1; i < len; i++) {
-      let newsBlock = document.createElement('div');
-      let np = document.createElement('h3');
-      let node = document.createTextNode(newsData[i].news_publisher);
-      np.appendChild(node);
-      np.classList.add('finCLI-Chrome-Publisher');
-
-      let nh = document.createElement('a');
-      let nh_text = document.createTextNode(
-        newsData[i].id + 1 + '. ' + newsData[i].news_headline
-      );
-      nh.classList.add('finCLI-Chrome-Headline');
-      let nh_link = newsData[i].news_link;
-      // console.log(typeof nh_link)
-      nh.href = nh_link;
-
-      nh.appendChild(nh_text);
-      newsBlock.append(np);
-      newsBlock.append(nh);
-
-      let element = document.getElementById('IN-News');
-      element.appendChild(newsBlock);
+async function postData() {
+  let response = await fetch('http://127.0.0.1:5000/get_news', {
+    method: 'POST',
+    cache: 'default',
+    body: JSON.stringify({"current_hour": new Date().toISOString()}),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
     }
+  })
+
+  return response.json()
+}
+
+function processAPIResponse(newsData) {
+  // console.log(newsData);
+  const headline = newsData[0].news_headline;
+  const newsLink = newsData[0].news_link;
+  const publisher = newsData[0].news_publisher;
+
+  const publisherElement = document.getElementById('publisherElement');
+  publisherElement.innerHTML = publisher;
+
+  const headlineElement = document.getElementById('headlineElement');
+  headlineElement.innerHTML = headline;
+  headlineElement.setAttribute('href', newsLink);
+
+  document.getElementById('newsIDElement').innerHTML =
+    newsData[0].id + 1 + '. ';
+
+  // adding further news headlines
+
+  var len = newsData.length;
+  console.log(len);
+
+  len = Math.min(len, 3);
+
+  for (var i = 1; i < len; i++) {
+    let newsBlock = document.createElement('div');
+    let np = document.createElement('h3');
+    let node = document.createTextNode(newsData[i].news_publisher);
+    np.appendChild(node);
+    np.classList.add('finCLI-Chrome-Publisher');
+
+    let nh = document.createElement('a');
+    let nh_text = document.createTextNode(
+      newsData[i].id + 1 + '. ' + newsData[i].news_headline
+    );
+    nh.classList.add('finCLI-Chrome-Headline');
+    let nh_link = newsData[i].news_link;
+    // console.log(typeof nh_link)
+    nh.href = nh_link;
+
+    nh.appendChild(nh_text);
+    newsBlock.append(np);
+    newsBlock.append(nh);
+
+    let element = document.getElementById('IN-News');
+    element.appendChild(newsBlock);
+  }
+}
+
+postData()
+  .then((res) => {
+    if (res.status === 'success') processAPIResponse(res.data.india);
   })
   .catch((Error) => console.log(Error));
